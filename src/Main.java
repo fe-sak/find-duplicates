7bcb30a122234ce0cb6e081473f1f8494f8e9bc3
Sakuma <felipe.sakuma@capco.com>
2023-01-11 19:58:19 -0300

    use enhanced for loop

--- src/Main.java
+++ src/Main.java
@@ -26,9 +26,10 @@ public class Main {
   public static List<String> findDuplicates(List<String> firstList, List<String> secondList){
     List<String> duplicates = new ArrayList<>();
 
-    for(int i = 0; i < firstList.size(); i++){
-      for(int j = 0; j < secondList.size(); j++){
-        if (Objects.equals(firstList.get(i), secondList.get(j))) duplicates.add(firstList.get(i));
+    for (String firstListElement : firstList) {
+      for (String secondListElement : secondList) {
+        if (Objects.equals(firstListElement, secondListElement))
+          duplicates.add(firstListElement);
       }
     }
     return duplicates;
