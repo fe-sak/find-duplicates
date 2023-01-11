1554c1295144d5a1f2d0a5764b7f7ed7c7117792
Sakuma <felipe.sakuma@capco.com>
2023-01-11 20:06:31 -0300

    change list initialization

--- src/FindDuplicates.java
+++ src/FindDuplicates.java
@@ -1,22 +1,16 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
 public class FindDuplicates {
 
   public static void main(String[] args) {
-    List<String> firstList = new ArrayList<>();
-    List<String> secondList = new ArrayList<>();
+    String[] firstArray = new String[]{"Morango", "Uva", "Acerola", "Manga"};
+    String[] secondArray = new String[]{"Morango", "Uva", "Acerola", "Manga"};
 
-    firstList.add("Morango");
-    firstList.add("Uva");
-    firstList.add("Acerola");
-    firstList.add("Manga");
-
-    secondList.add("Pêra");
-    secondList.add("Caju");
-    secondList.add("Morango");
-    secondList.add("Kiwi");
+    List<String> firstList = Arrays.asList(firstArray);
+    List<String> secondList = Arrays.asList(secondArray);
 
     List<String> duplicates = findDuplicates(firstList, secondList);
 
