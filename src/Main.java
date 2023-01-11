eb8c4e25b37b75ce1a37c779ec61f26eae775ff9
Sakuma <felipe.sakuma@capco.com>
2023-01-11 19:49:05 -0300

    move method find to Main Class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Main {
 
@@ -17,8 +18,19 @@ public class Main {
     secondList.add("Morango");
     secondList.add("Kiwi");
 
-    List<String> duplicates = DuplicatesFinder.find(firstList, secondList);
+    List<String> duplicates = findDuplicates(firstList, secondList);
 
     System.out.println(duplicates);
+  }
+
+  public static List<String> findDuplicates(List<String> firstList, List<String> secondList){
+    List<String> duplicates = new ArrayList<>();
+
+    for(int i = 0; i < firstList.size(); i++){
+      for(int j = 0; j < secondList.size(); j++){
+        if (Objects.equals(firstList.get(i), secondList.get(j))) duplicates.add(firstList.get(i));
+      }
+    }
+    return duplicates;
   }
 }
